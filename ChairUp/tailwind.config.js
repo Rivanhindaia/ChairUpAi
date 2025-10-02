edbb04f502de293a71042e1f75326d61d5d87e00
Rivanhindaia <rivanhindaia4@gmail.com>
2025-10-01 19:42:52 -0700

    Update tailwind.config.js

--- ChairUp/tailwind.config.js
+++ ChairUp/tailwind.config.js
@@ -1,12 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+const config: Config = {
   content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        brand: { DEFAULT: '#0ea5e9', dark: '#0369a1' }
-      }
-    }
-  },
+  theme: { extend: {} },
   plugins: []
 }
+export default config
