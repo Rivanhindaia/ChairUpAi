34b305c35a53bd31b5a8d8ea8c8fa465f8d6350f
Rivanhindaia <rivanhindaia4@gmail.com>
2025-10-01 19:30:29 -0700

    Create Nav.tsx

---
+++ ChairUp/components/Nav.tsx
@@ -0,0 +1,4 @@
+<Link href="/" className="flex items-center gap-2">
+  <div className="h-9 w-9 rounded-2xl bg-black text-white flex items-center justify-center font-bold">Cu</div>
+  <div className="logo text-lg">Chair<span className="accent">Up</span></div>
+</Link>
